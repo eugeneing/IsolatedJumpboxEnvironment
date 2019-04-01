a59fed2b2bfe31661e072bd9c51b3f14f363748c
Eugene Ng (Business Guest) <b-eung@microsoft.com>
2019-04-01 09:42:06 -0700

    encryption key supports both bring your own key and also azure created key with prompts when neither is explicitly chosen.

--- data_isolation/enckey.ps1
+++ data_isolation/enckey.ps1
@@ -1,3 +1,4 @@
+[CmdletBinding(DefaultParameterSetName='none')]
 param(
     [Parameter(Mandatory=$true)]
     [string]$ResourceGroup,
@@ -6,7 +7,15 @@ param(
     [Parameter(Mandatory=$true)]
     [string]$KeyvaultName,
     [bool]$AzureOutput = $false,
-    [string]$StorageKey
+    [string]$StorageKey,
+    [Parameter(ParameterSetName='BYOK')]
+    [string]$BYOKFile,
+    [Parameter(ParameterSetName='PEM')]
+    [string]$PEMFile,
+    [Parameter(ParameterSetName='SPEM')]
+    [string]$ProtectedPEMFile,
+    [Parameter(ParameterSetName="AZURE")]
+    [switch]$AzureCreatedKey
 )
 
 if($AzureOutput){
@@ -15,6 +24,11 @@ if($AzureOutput){
     $outputArg = "-o"+"none"
 }
 
+$keyType = ""
+$custKeyType = ""
+$filename = ""
+$PEMPass = ""
+
 if (!$StorageKey){
     $StorageKey = "StrgEncKey1"
 }
@@ -22,11 +36,88 @@ if (!$StorageKey){
 # List Versions of Key and store based on name of $StorageKey and $KeyvaultName
 $key = az keyvault key list-versions --vault-name $KeyvaultName -n $StorageKey --query "[?contains(kid, '$StorageKey') ].kid" -o tsv
 
-# If a key does not exist then create key based on the informatio provided by $stroagekey 
+# If a key does not exist then create key based on the information provided by $stroagekey 
 if (!$key){
-    Write-Host "Creating key $StorageKey in Keyvault $KeyvaultName"
-    az keyvault key create --vault-name $KeyvaultName -n $StorageKey --protection software $outputArg
+    Write-Host "Key $StorageKey was not found in the Keyvault $KeyvaultName"
+    
+    Do {
+        # Check if any of our flags for imported keys are present. 
+        if ($BYOKFile -or $PEMFile -or $ProtectedPEMFile){
+            $keyType = "2"
+        }elseif($AzureCreatedKey){ # Check if we just want an Azure Created Key
+            $keyType = "1"
+        }
+        else{ # Case where we are not provided information about type of key desired.
+            $keyType = Read-Host -Prompt "Would you like $StorageKey to be [1] Azure created or [2] Import a key?"
+        }
 
+        # Invalid input case
+        if(!(($keyType -eq "1") -or ($keyType -eq "2") -or (!$keyType))){
+            Write-Host "Invalid Input of `"$keyType`" provided.`n`n Valid input is either 1 or 2.`n"
+        } elseif ($keyType -eq "1"){ # If we want an Azure Created Key
+            Write-Host "Creating key $StorageKey in Keyvault $KeyvaultName"
+            az keyvault key create --vault-name $KeyvaultName -n $StorageKey --protection software $outputArg
+        } elseif ($keyType -eq "2"){ # If one of the imported keys is parameterized or user selected to import keys
+            Do{ 
+                # If one of the parameters provided
+                if ($BYOKFile -or $PEMFile -or $ProtectedPEMFile){
+                    if ($BYOKFile){
+                        $custKeyType = "1"
+                        $filename = $BYOKFile
+                    } elseif ($PEMFile){
+                        $custKeyType = "2"
+                        $filename = $PEMFile
+                    } elseif ($ProtectedPEMFile){
+                        $custKeyType = "3"
+                        $filename = $ProtectedPEMFile
+                    }
+                } else { # Not one of the preselected parameters so prompt user for information.
+                    $custKeyType = Read-Host -Prompt "Are you importing a [1] Non-password protected BYOK file [2]  Non-password protected PEM file or [3] Password protected PEM?"
+                }
+
+                if (!(($custKeyType -eq "1") -or ($custKeyType -eq "2") -or ($custKeyType -eq "3"))){ # Check for invalid input
+                    Write-Host "Invalid Input of `"$custKeyType`" provided.`n`n Valid input is either 1,2,3.`n"
+                } elseif ($custKeyType -eq "1"){ # Selected 1 based on prompt for a non-pass protected BYOK file
+                    while(!$filename){
+                        $filename = Read-Host -Prompt "Please enter the non-password protected BYOK's full filename including the path"
+                        if(!$filename){
+                            Write-Host "Filename cannot be an empty string"
+                        }
+                    }
+
+                    Write-Host "Importing non-password protected key file `'$filename`' as key `'$StorageKey`' in Keyvault `'$KeyvaultName`'"
+                    az keyvault key import --vault-name $KeyvaultName -n $StorageKey --byok-file $filename --protection software $outputArg
+                } elseif ($custKeyType -eq "2"){ # Selected 2 based on prompt for a non-pass protected PEM file
+                    while(!$filename){
+                        $filename = Read-Host -Prompt "Please enter non-password protected PEM's full filename including the path"
+                        if(!$filename){
+                            Write-Host "Filename cannot be an empty string"
+                        }
+                    }
+    
+                    Write-Host "Importing non-password protected PEM file `'$filename`' as key `'$StorageKey`' in Keyvault `'$KeyvaultName`'"
+                    az keyvault key import --vault-name $KeyvaultName -n $StorageKey --pem-file $filename --protection software $outputArg
+                } elseif ($custKeyType -eq "3"){ # Selected 3 based on prompt for a pass protected PEM file
+                    while(!$filename){
+                        $filename = Read-Host -Prompt "Please enter the password protected PEM's full filename including the path"
+                        if(!$filename){
+                            Write-Host "Filename cannot be an empty string"
+                        }
+                    }
+                    
+                    while(!$PEMPass){ # Get User to input password for the password protected PEM file
+                        $PEMPass = Read-Host -Prompt "Please enter password for the PEM file $filename" -AsSecureString
+                        if(!$PEMPass){
+                            Write-Host "Passowrd cannot be an empty string"
+                        }
+                    }
+
+                    Write-Host "Importing password protected PEM file `'$filename`' as key `'$StorageKey`' in Keyvault `'$KeyvaultName`'"
+                    az keyvault key import --vault-name $KeyvaultName -n $StorageKey --pem-file $filename --pem-password $([Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($PEMPass))) --protection software $outputArg
+                }
+            } while(!(($custKeyType -eq "1") -or ($custKeyType -eq "2") -or ($custKeyType -eq "3")))
+        }
+    } while(!(($keyType -eq "1") -or ($keyType -eq "2")))
     $key = az keyvault key list-versions --vault-name $KeyvaultName -n $StorageKey --query "[?contains(kid, '$StorageKey') ].kid" -o tsv
 }
 
